aa42fb42f354b4f61e66488c98892bdb939fb0cd
Mike C <mikecaiazzo23@gmail.com>
2016-04-21 10:02:31 -0400

    Using require and include for added file structure

---
+++ advanced-function-and-commands/include_require.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html>
+<head>
+    <title>PHP Functions!</title>
+</head>
+<body>
+<?php
+include("include_this_file.html"); // can also use include_once to include a file only once ina  project
+require("require_this_file.html"); // can also use require_once to only require a file one time throughout a project
+?>
+</body>
+</html>
