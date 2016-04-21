3dabd177a57671dfd4187d07194edcb50ed72e7f
Mike C <mikecaiazzo23@gmail.com>
2016-04-20 23:56:30 -0400

    Added PHP file that gets email user inputs into form and outputs it on submit

---
+++ intro-to-php/echo_print.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<h2>Hello World</h2>';
+
+print "This is an example of another string using the 'print' command<br/>";
+
+/** @var TYPE_NAME $array */
+$array = ["name","city","state"];
+
+print_r($array);
