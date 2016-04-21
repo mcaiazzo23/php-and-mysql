761184d9451fc75a80202f4c3ceb20722cd86d2a
Mike C <mikecaiazzo23@gmail.com>
2016-04-21 08:56:47 -0400

    Trying out arrays

---
+++ intro-to-php/arrays.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html>
+<head>
+    <title>PHP Array!</title>
+</head>
+<body>
+<?php
+$name = "Michael Caiazzo"
+$name_more = array("Mike","Mikey","Mikey Boi","MAC Daddy");
+
+echo $name_more[0];
+?>
+</body>
+</html>
